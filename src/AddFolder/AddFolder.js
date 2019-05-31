35ac4f52b1f5b1f4ff9238aa9f8f0402af3183c0
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-31 12:48:58 -0400

    started to flesh out AddFolder. added style to +Add button

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './AddFolder.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class AddFolder extends React.Component {
+
+  state = {
+    folderName: '',
+    error: null
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+    }
+    
+
   render() {
     return (
       <form className="AddFolder">
         <label htmlFor="folder-name">Folder Name:</label>
         <input type="text" name="folder-name" id="folder-name"/>
-        <button type="submit">Add new folder</button>
-        <button class="cancel-button">Cancel</button>
+        <button type="submit">
+          <FontAwesomeIcon icon='plus' />
+          <br />
+          Add
+        </button>
       </form>
     )
   }
